e28f0e343b83f76383a016b5bfb03cef95c2e205
SCJ2798 <chathuranga9827@gmail.com>
2022-11-03 21:00:00 +0530

    update_code

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:foodx/pages/home_page.dart';
 
+// void main() {
+//   runApp(DevicePreview(
+//     enabled: true,
+//     isToolbarVisible: false,
+//     builder: (context) => const MyApp(),
+//   ));
+// }
+
 void main() {
-  runApp(DevicePreview(
-    enabled: true,
-    isToolbarVisible: false,
-    builder: (context) => const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
